be191d7d8237b39714bfb944c1933f6d1f268344
hasa <266087079+hasa-ui@users.noreply.github.com>
2026-04-02 18:47:13 +0900

    Update runbot.sh

--- runbot.sh
+++ runbot.sh
@@ -1,10 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
 cd /data/data/com.termux/files/home/discord-bot || exit 1
-
-git fetch origin || exit 1
-git checkout main || exit 1
-git reset --hard origin/main || exit 1
-
 . /data/data/com.termux/files/home/discord-bot/setenv.sh
 exec python /data/data/com.termux/files/home/discord-bot/bot.py
